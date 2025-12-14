92bec0e67770b28d42efac611c7116ceb8deb7c7
Aadarsh Yadav <aadarshyadav1250@gmail.com>
2025-12-14 19:37:34 +0530

    feat: more concise and clean code by separating all routes

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express')
+// const {users} = require('./data/users.json');
+// const {books} = require('./data/books.json');
+
+const usersRouter = require('./routes/users.js');
+const booksRouter = require('./routes/books.js')
 
 const app = express();
 
+//middleware to pass JSON bodies
+// this allows us to handle JSON data sent in requests 
+app.use(express.json())
 
+app.use('/users', usersRouter);
+app.use('/books', booksRouter);
 
 app.get('/',(req,res)=>{
     res.status(200).json({
@@ -15,6 +25,7 @@ app.get('/',(req,res)=>{
 //         message: "Not implemented Yet !!"
 //     })
 // })
+
 
 
 const PORT = 8080;
